d194fc4b7d3f6fcd6ec447a258a31486c254d856
Lucas Zani <lucas.z.luz@aluno.senai.br>
2025-03-27 10:40:11 -0300

    feat: create BookList archive

---
+++ data/BookList.js
@@ -0,0 +1,34 @@
+export default Livros = [
+    {
+        link:"#",
+        title:"Pequeno príncipe",
+        autor:"Antoine de Saint-Exupéry",
+        stars: 4.5,
+        principal_gender: "Infantil",
+        secondary_gender: "Fantasia científica"
+    },
+    {
+        link:"#",
+        title:"A culpa é das estrelas",
+        autor:"John Green",
+        stars: 4.8,
+        principal_gender: "Romance",
+        secondary_gender: "Comédia"
+    },
+    {
+        link:"#",
+        title:"1984",
+        autor:"George Orwell",
+        stars: 4.7,
+        principal_gender: "Ficção científica",
+        secondary_gender: "Ficção distópica"
+    },
+    {
+        link:"#",
+        title:"The cats of Uthar",
+        autor:"H. P. Lovecraft",
+        stars: 4.1,
+        principal_gender: "Conto",
+        secondary_gender: "Terror"
+    }
+]
