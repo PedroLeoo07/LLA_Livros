3bf8f232765dcea8f7d39e5dfc897a53f581ba6a
Lucas Zani <lucas.z.luz@aluno.senai.br>
2025-04-03 09:57:12 -0300

    fix: add header

---
+++ components/header.jsx
@@ -0,0 +1,23 @@
+import styles from '../styles/Header.module.css';
+export default function Header() {
+    return (
+        <div>
+            <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px', backgroundColor: '#7d93f5' }}>
+
+                <h1 className={styles.h1}>LivrariaLLA 📚</h1>
+                
+                <input 
+                    type="text" 
+                    placeholder="Pesquisar" 
+                    style={{ padding: '5px', fontSize: '16px', width: '200px', borderRadius: '10px', border: '10px' }} 
+                />
+                
+                <nav style={{ display: 'flex', justifyContent: 'center', gap: '20px', padding: '20px', backgroundColor: '#7d93f5' }}>
+                <a href="#inicio" style={{ textDecoration: 'none', color: '#FFFF' }}>Início</a>
+                <a href="#destaque" style={{ textDecoration: 'none', color: '#FFFF  ' }}>Destaque da Semana</a>
+                <a href="#categorias" style={{ textDecoration: 'none', color: '#FFFF' }}>Categorias</a>
+            </nav>
+            </header>
+        </div>
+    );
+}
