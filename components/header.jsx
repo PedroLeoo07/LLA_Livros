98c74cc1981bac8c3f86cc462887e66f7984a517
André Lucca <157771933+AndreLucca19@users.noreply.github.com>
2025-04-03 08:55:24 -0300

    update header

--- components/header.jsx
+++ components/header.jsx
@@ -4,25 +4,18 @@ export default function Header() {
         <div>
             <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px', backgroundColor: '#7d93f5' }}>
 
-                <h1>LivrariaLLA 📚</h1>
-
-                <h1 className={styles.h1}>LivrariaLLA</h1>
-                <nav style={{ display: 'flex', justifyContent: 'center', gap: '20px', padding: '20px', backgroundColor: '#7d93f5' }}>
-                <a href="#inicio" style={{ textDecoration: 'none', color: '#FFFF' }}>Início</a>
-                <a href="#destaque" style={{ textDecoration: 'none', color: '#FFFF  ' }}>Destaque da Semana</a>
-                <a href="#categorias" style={{ textDecoration: 'none', color: '#FFFF' }}>Categorias</a>
-            </nav>
-
+                <h1 className={styles.h1}>LivrariaLLA 📚</h1>
+                
                 <input 
                     type="text" 
                     placeholder="Pesquisar" 
                     style={{ padding: '5px', fontSize: '16px', width: '200px', borderRadius: '10px', border: '10px' }} 
                 />
-
-                <nav style={{ display: 'flex', justifyContent: 'center', gap: '20px', padding: '10px', backgroundColor: '#7d93f5' }}>
-                <a href="#inicio" style={{ textDecoration: 'none', color: '#333' }}>Início</a>
-                <a href="#destaque" style={{ textDecoration: 'none', color: '#333' }}>Destaque da Semana</a>
-                <a href="#categorias" style={{ textDecoration: 'none', color: '#333' }}>Categorias</a>
+                
+                <nav style={{ display: 'flex', justifyContent: 'center', gap: '20px', padding: '20px', backgroundColor: '#7d93f5' }}>
+                <a href="#inicio" style={{ textDecoration: 'none', color: '#FFFF' }}>Início</a>
+                <a href="#destaque" style={{ textDecoration: 'none', color: '#FFFF  ' }}>Destaque da Semana</a>
+                <a href="#categorias" style={{ textDecoration: 'none', color: '#FFFF' }}>Categorias</a>
             </nav>
             </header>
         </div>
