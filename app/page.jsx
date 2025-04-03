684370de0bdbe34497822148d8a2ffdf80ed9c61
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-04-03 08:15:21 -0300

    style: correção de cores

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,22 @@
+import styles from "../styles/Card4.module.css";
 
-export default function Header() {
+export default function banner() {
     return (
-        <div>
-            <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px', backgroundColor: '#f4f4f4' }}>
-                <h1>LivrariaLLA</h1>
-                <input 
-                    type="text" 
-                    placeholder="Pesquisar" 
-                    style={{ padding: '5px', fontSize: '16px', width: '200px', borderRadius: '15px', border: '10px' }} 
-                />
-                <nav style={{ display: 'flex', justifyContent: 'center', gap: '20px', padding: '10px', backgroundColor: '#f4f4f4' }}>
-                <a href="#inicio" style={{ textDecoration: 'none', color: '#333' }}>Início</a>
-                <a href="#destaque" style={{ textDecoration: 'none', color: '#333' }}>Destaque da Semana</a>
-                <a href="#categorias" style={{ textDecoration: 'none', color: '#333' }}>Categorias</a>
-            </nav>
-            </header>
-        </div>
-    );
+        <div className={styles.div1}>
+            <div className={styles.div2}>
+                <h2 className={styles.h2}>Fique por dentro das novidades!</h2>
+                <p className={styles.p}>
+                    Assine nossa newsletter e receba atualizações sobre novos lançamentos, promoções exclusivas e dicas de leitura.
+                </p>
+                <div className={styles.div3}>
+                    <input
+                        type="email"
+                        placeholder="Seu melhor e-mail"
+                        className={styles.input}
+                    />
+                    <button className={styles.button}>Entrar</button>
+                </div>
+            </div>
+        </div>  
+    )
 }
-
-
