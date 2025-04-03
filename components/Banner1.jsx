b5d284915492db12266c214d085fbdda847da805
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-04-03 09:03:01 -0300

    Create Banner1

---
+++ components/Banner1.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/Banner1.module.css";  
+
+export default function Banner1() {
+    return (
+        <div className={styles.div12}>
+            <div className={styles.div22}>
+                <h2 className={styles.h22}>Fique por dentro das novidades!</h2>
+                <p className={styles.p2}>Aproveite as nossas promoções com até 30% de desconto</p>
+            </div>
+        </div>
+    )
+}
